51d84d26b7da5c1c658e1276669000b7100670d4
NAME <VanoGalen@yandex.ru>
2023-02-19 22:13:07 +0300

    optimized code even more in index.html

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@
 git config --global user.name 'NAME'
 git config --global user.email 'VanoGalen@yandex.ru'
 git add *
-git commit -m "new pycharm proj"
+git commit -m "optimized code even more in index.html"
 git branch -M main
 git remote add origin https://github.com/Jakie-boah/sellingsite.git
 git push -u origin main
