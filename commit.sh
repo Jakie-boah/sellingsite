e08122fb6771d99133f482dba770f86291c17557
NAME <VanoGalen@yandex.ru>
2023-02-26 13:04:17 +0300

    done aa

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@ datetime_now=`date +'%Y-%m-%d %H:%M:%S'`
 git config --global user.name 'NAME'
 git config --global user.email 'VanoGalen@yandex.ru'
 git add *
-git commit -m "updated aa and added new favs filters"
+git commit -m "done aa"
 git branch -M main
 git remote add origin https://github.com/Jakie-boah/sellingsite.git
 git push -u origin main
